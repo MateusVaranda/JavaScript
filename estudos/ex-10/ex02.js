9fcfc25aa38a71f589bb1dfe7e6042c210365429
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-01-21 15:23:52 -0300

    ESTUDOS EM CONDIÇÕES JS
    IF
    ELSE

---
+++ estudos/ex-10/ex02.js
@@ -0,0 +1,7 @@
+var idaddepessoa = 18;
+console.log("Idade da pessoa: " + idaddepessoa);
+if (idaddepessoa >= 18){
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
