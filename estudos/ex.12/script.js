6477ea3dfba161d28184ac0351b7b16bbcdfad04
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-01-23 21:36:24 -0300

    Estrutura básicas de Repetições
    Repetições com while e do

---
+++ estudos/ex.12/script.js
@@ -0,0 +1,25 @@
+var pizza = 1
+do {
+    console.log("Estou comendo a fatia " + pizza + " de pizza")
+    pizza++
+} while (pizza <= 8)
+
+
+
+/* 
+var pizza = 1
+while (pizza <= 8) {
+    console.log("Estou comendo a fatia " + pizza + " de pizza")
+    pizza++
+}
+*/
+
+
+/*
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+console.log("6")
+*/
