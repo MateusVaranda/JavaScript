ecfb67a410a3dc70687507c74360ec0fa2d5a7f1
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-01-26 15:41:56 -0300

    Estudo modo de depuração

---
+++ estudos/ex.13/depuração1.js
@@ -0,0 +1,16 @@
+console.log('Vai iniciar...')
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('Fim!')
+
+// Código acima é equivalente a este abaixo:
+/*
+console.log('Vai iniciar...')
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}   
+console.log('Fim!')
+*/
---
+++ estudos/ex.13/script.js
@@ -0,0 +1,14 @@
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(c)
+    c++
+}
+*/
+
+
+for(var c = 1; c <= 6; c++) { //estrutura de repetição com variável de controle
+    console.log(c) //bloco de código
+}   
